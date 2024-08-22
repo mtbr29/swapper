373e34a2682c8d58a82da9218e3718b030790fba
mtbr29 <157277351+mtbr29@users.noreply.github.com>
2024-08-22 20:29:26 +0200

    Create example.bat

---
+++ example.bat
@@ -0,0 +1,8 @@
+echo made by mtbr29 and jim laz:
+echo this will be replace 8C 00  by 8C 01 at pakchunk10-WindowsClient_s2.ucas
+pause
+echo it can take some time
+Swapper.exe "C:\Program Files\Epic Games\fortnite\fortnitegame\content\paks\pakchunk10-WindowsClient_s2.ucas" "8C 00" "8C 01"
+done!
+pause
+exit
